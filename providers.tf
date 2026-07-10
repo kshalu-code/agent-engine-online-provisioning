51704507b74c3b2d210853b5b40fc3b3a00b6850
kshalu <kshalu@google.com>
2026-07-10 09:43:58 +0000

    Simplify providers.tf to just provider declarations

--- providers.tf
+++ providers.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "google" {}
 
 provider "google-beta" {}
